76dc38bd39324c3769fe51e6121547b5bc3de9d5
Romain Francois <romain@tada.science>
2024-04-06 20:11:41 +0200

    + slap()

--- R/slap.R
+++ R/slap.R
@@ -24,11 +24,13 @@
 #' @name slap
 #' @export
 `%!%` <- function(expr, message) {
+  message <- enexpr(message)
+
   quo <- quo(
     withCallingHandlers(
       {{ expr }},
-      error = function(err) {
-        message <- {{ message }}
+      error = function(.__slap__.) {
+        message <- !!message
 
         error_call <- env$.__error_call__.
         if (is.null(error_call)) {
@@ -38,11 +40,16 @@
           error_call <- NULL
         }
 
-        cli::cli_abort(message, parent = err, call = error_call)
+        cli::cli_abort(message, parent = .__slap__., call = error_call)
       }
     )
   )
 
   env <- caller_env()
   eval_tidy(quo, env = env, data = list(env = env))
+}
+
+#' @export
+slap <- function(error_call = current_env()) {
+  get(".__slap__.", caller_env()) %||% cli_abort("{.fn slap()} can only be used on the rhs of a `%!%`.", call = error_call)
 }
